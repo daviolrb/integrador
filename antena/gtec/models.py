a7f61f3719e60f0ca03fb00bebcf905c00cca775
daviolrb <33334853+daviolrb@users.noreply.github.com>
2020-03-18 16:34:50 -0300

    Update models.py

--- antena/gtec/models.py
+++ antena/gtec/models.py
@@ -3,23 +3,6 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 import uuid
 
-class Post(models.Model):
-
-	POST_STATUS = (
-		('active', 'Ativo'),
-		('draft', 'Rascunho')
-	)
-	title = models.CharField(max_length=200)
-	slug = models.SlugField(max_length=200)
-	body = models.TextField()
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	created_at = models.DateField(auto_now_add=True)
-	updated_at = models.DateField(auto_now=True)
-	status = models.CharField(max_length=15, choices=POST_STATUS)
-
-	def __str__(self):
-	    return self.title
-
 class Vaga(models.Model):
     id = models.IntegerField('ID', primary_key = True, help_text= 'ID único para essa vaga') # editable = False 'esconde' o campo do id, evitando que ele seja editado
     hora_dev = models.DateField('Hora de Devolução', null = True, blank = True)
