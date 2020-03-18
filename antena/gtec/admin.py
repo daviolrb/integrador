74ec8362cfd2c143a7cf51cc5674e99025d469a6
daviolrb <33334853+daviolrb@users.noreply.github.com>
2020-03-18 16:35:37 -0300

    Update admin.py

--- antena/gtec/admin.py
+++ antena/gtec/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-from .models import Post, Vaga, Cliente, Reserva
+from .models import Vaga, Cliente, Reserva
 
-class PostAdmin(admin.ModelAdmin):
-    list_display  = ('title','author', 'created_at', 'updated_at', 'status')
-    search_fields = ['title']
-    prepopulated_fields = {"slug": ("title",)}
-
-admin.site.register(Post, PostAdmin)
 admin.site.register(Vaga)
 admin.site.register(Reserva)
 admin.site.register(Cliente)
